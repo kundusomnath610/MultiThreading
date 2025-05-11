5993c3c5124873866975ec6db911dbd9aebb3030
Somnath Kundu <kundusomnath610@gmail.com>
2025-05-11 13:10:19 +0530

    first commit

---
+++ src/main/java/org/example/thread/World.java
@@ -0,0 +1,23 @@
+package org.example.thread;
+
+public class World extends  Thread{
+    @Override
+    public void run() {
+        System.out.println("RUNNABLE");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        World world = new World();
+        System.out.println(world.getState());
+        world.start();
+        System.out.println(world.getState());
+        world.sleep(200);
+        System.out.println(world.getState());
+    }
+}
